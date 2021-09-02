8767ab6ceae7ee1066ae60ff34f0c1b01c4a4bb0
nvh-gel <nvhien2703@outlook.com>
2021-09-03 00:03:58 +0700

    add header and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoComponent } from './component/core/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
