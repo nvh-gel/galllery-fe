c6b8a26c3c5e9270ac4536a9bd8d580ec85ae1b6
nvh-gel <nvhien2703@outlook.com>
2021-09-11 03:05:48 +0700

    GL-55 refactor

--- server.js
+++ server.js
@@ -4,18 +4,14 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
-
 
 // Serve only static filles from dist directory
 app.use(express.static('./dist/gallery-fe/'))
 
-
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/gallery-fe/'}),
 );
-
 
 // start the app by listening on default heroku port
 app.listen(process.env.PORT || 8080);
